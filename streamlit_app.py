24b1e9245dbcac126f5431b5a141a37fb7389468
kamiya <kamiya.yoshiyuki0604@gmail.com>
2025-05-03 23:54:04 +0900

    app_rev

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,22 +13,22 @@ uploaded_file = st.file_uploader("画像を選択してください", type=["jpg
 if uploaded_file is not None:
     # 画像を表示
     image = Image.open(uploaded_file)
-    st.image(image, caption="アップロードされた画像", use_column_width=True)
+    st.image(image, caption="アップロードされた画像",  use_container_width=True)
 
     # APIにリクエストを送信
     if st.button("物体検出を実行"):
-        st.spinner("物体検出を実行中...")
         files = {
         "file": (uploaded_file.name, uploaded_file.getvalue(), uploaded_file.type)
         }
-        try:
-            response = requests.post(API_ENDPOINT, files=files)
-            response.raise_for_status()  # エラーレスポンスの場合に例外を発生させる
+        with st.spinner("物体検出を実行中..."):
+            try:
+                response = requests.post(API_ENDPOINT, files=files)
+                response.raise_for_status()  # エラーレスポンスの場合に例外を発生させる
 
-            # 結果画像を表示
-            result_image = Image.open(io.BytesIO(response.content))
-            st.image(result_image, caption="物体検出の結果", use_column_width=True)
-        except requests.exceptions.RequestException as e:
-            st.error(f"APIリクエストエラー: {e}")
-        except Exception as e:
-            st.error(f"エラー: {e}")
+                # 結果画像を表示
+                result_image = Image.open(io.BytesIO(response.content))
+                st.image(result_image, caption="物体検出の結果", use_column_width=True)
+            except requests.exceptions.RequestException as e:
+                st.error(f"APIリクエストエラー: {e}")
+            except Exception as e:
+                st.error(f"エラー: {e}")
