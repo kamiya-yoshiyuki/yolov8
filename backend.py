031054331f27e9967361fd6f8ebc3c27a2d2dc22
kamiya <kamiya.yoshiyuki0604@gmail.com>
2025-05-04 00:24:43 +0900

    backend

--- backend.py
+++ backend.py
@@ -166,4 +166,4 @@ if __name__ == "__main__":
     print("FastAPIサーバーを起動します (http://127.0.0.1:8000)")
     # Uvicornサーバーを起動
     # reload=True は開発時に便利だが、本番環境では False にする
-    uvicorn.run("backend:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("backend:app", host="0.0.0.0", port=8000, reload=True)
